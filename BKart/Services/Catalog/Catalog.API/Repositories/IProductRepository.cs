14d31abae7aa78f6bc2d2cb5268702e93e3e8212
bhuvan raj <bhuvan.raj.r@gmail.com>
2022-03-10 23:22:41 +0530

    Catalog API

---
+++ BKart/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<Product> GetProductByName(string name);
+        Task<List<Product>> GetProductsByCategory(string category);
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+
+    }
+}

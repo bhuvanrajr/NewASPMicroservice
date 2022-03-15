0539cb376e066c7b2c754c0209fa670adb204fa9
bhuvan raj <bhuvan.raj.r@gmail.com>
2022-03-16 00:48:15 +0530

    Discount.API

---
+++ BKart/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,51 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _discountRepository;
+        public DiscountController(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository;
+        }
+
+        [HttpGet]
+        [Route("[action]/{productName}")]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            return Ok(await _discountRepository.GetDiscount(productName));
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            await _discountRepository.CreateDiscount(coupon);
+            return Ok(await _discountRepository.GetDiscount(coupon.ProductName));
+        }
+
+        [HttpPut]
+        [Route("[action]")]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            await _discountRepository.UpdateDiscount(coupon);
+            return Ok(await _discountRepository.GetDiscount(coupon.ProductName));
+        }
+
+        [HttpGet]
+        [Route("[action]/{productName}")]
+        public async Task<ActionResult<bool>> DeleteDiscount(string productName)
+        {
+            return Ok(await _discountRepository.DeleteDiscount(productName));
+        }
+    }
+}
---
+++ BKart/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Entities
+{
+    [Table("coupon")]
+    public class Coupon
+    {
+        [Column("id")]
+        public int ID { get; set; }
+        [Column("productname")]
+        public string ProductName { get; set; }
+        [Column("description")]
+        public string Description { get; set; }
+        [Column("amount")]
+        public int Amount { get; set; }
+    }
+}
